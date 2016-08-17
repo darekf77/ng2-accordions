337c05837fd8347a68980dc61251576a6f89a5b2
Dariusz <darekf77@gmail.com>
2016-08-17 17:37:24 +0200

    Release 0.0.5

--- src/bs3-breakpoints.enum.ts
+++
@@ -1,3 +0,0 @@
-export enum Bs3BreakPoint {
-    xs, sm, md, lg
-}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export * from './bs3-breakpoints.component';
-export * from './bs3-breakpoints.enum';
-export * from './size.enum';
+export * from './accordion.component';
+export * from './accordion-group';
